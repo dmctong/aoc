1b58d71655bea5f8d7220ab949423404fa463b4e
Dominic Tong <dominic@Dominics-Laptop.local>
2024-05-31 00:08:19 -0700

    cleanup part 1

--- 2019d6/p1.py
+++ 2019d6/p1.py
@@ -1,20 +1,13 @@
 from collections import deque
 from collections import defaultdict
-from itertools import chain
 
 filename = 'e.txt'
-filename = 'e2.txt'
 
 graph = defaultdict(list)
-# all_nodes = set()
 with open(filename, 'r') as file:
     for line in file:
         line = line.strip().split(')')
         graph[line[0]].append(line[1])
-        # all_nodes.add(int(line[0]))
-
-# print(graph)
-# print(all_nodes)
 
 def dfs(graph, start):
     all_paths = defaultdict(list)
@@ -37,33 +30,6 @@ def dfs(graph, start):
 all_paths = dfs(graph, 'COM')
 orbits = 0
 for key, value in enumerate(all_paths):
-    # print(key)
-    # print(value)
     orbits += len(*all_paths[value])-1
-    # print(*all_paths[value])
 
 print(f'Part 1: there are {orbits} direct and indirect orbits.')
-
-print(graph)
-
-def bfs(graph, start, end):
-    queue = deque()
-    queue.append([start])
-    visited = set()
-    visited.add(start)
-
-    while queue:
-        path = queue.popleft()
-        s = path[-1]
-
-        if s == end:
-            final_path = path
-            break
-        for n in graph[s]:
-            copy_path = path[:]
-            copy_path.append(n)
-            queue.append(copy_path)
-            visited.add(n)
-    return(final_path)
-
-print(bfs(graph, 'YOU', 'SAN'))
